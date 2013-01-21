277869bf6d1fd5a8ff594eb23bdacdd5df67e79c
David Rawlinson <davidjr@400L-77202.unimelb.edu.au>
2013-01-21 16:09:09 +1100

    tested working batch file

--- bin/TrackAssist.bat
+++ bin/TrackAssist.bat
@@ -1,14 +1,11 @@
+@echo off
 setlocal
 
-REM set paths to these locations:
-set APACHE="C:\Program Files (x86)\NetBeans 6.9.1\ide\modules\ext"
-set HIBERN="C:\Program Files (x86)\NetBeans 6.9.1\java\modules\ext\hibernate"
-set POSTGR="C:\Program Files (x86)\NetBeans 6.9.1\ide\modules\ext"
-set LIB="C:\Users\davidjr\Documents\NetbeansProjects\jars\LIB"
-
-set JAVAPATH= "C:\Program Files (x86)\Java\jdk1.6.0_23\bin"
-set APPPATH="C:\Users\davidjr\Documents\NetbeansProjects\CellTracking\dist"
+REM hibernate libs included for easy setup on non-dev machines:
+set HIBERNATE="..\lib"
 
 REM Add extra memory to run app:
-%JAVAPATH%\java -Xmx1024m -cp %APACHE%\commons-logging-1.1.jar;%HIBERN%\antlr-2.7.6.jar;%HIBERN%\asm.jar;%HIBERN%\asm-attrs.jar;%HIBERN%\cglib-2.1.3.jar;%HIBERN%\commons-collections-2.1.1.jar;%HIBERN%\dom4j-1.6.1.jar;%HIBERN%\ehcache-1.2.3.jar;%HIBERN%\jdbc2_0-stdext.jar;%HIBERN%\jta.jar;%HIBERN%\hibernate3.jar;%HIBERN%\hibernate-tools.jar;%HIBERN%\hibernate-annotations.jar;%HIBERN%\hibernate-commons-annotations.jar;%HIBERN%\hibernate-entitymanager.jar;%HIBERN%\javassist.jar;%HIBERN%\ejb3-persistence.jar;%POSTGR%\postgresql-8.3-603.jdbc3.jar;%LIB%\ij.jar;%LIB%\ujmp-complete-0.2.5.jar;%APPPATH%\CellTracking.jar au.com.nicta.ct.desktop.Main
+REM %JAVAPATH%\java -Xmx1024m -cp %APACHE%\commons-logging-1.1.jar;%HIBERN%\antlr-2.7.6.jar;%HIBERN%\asm.jar;%HIBERN%\asm-attrs.jar;%HIBERN%\cglib-2.1.3.jar;%HIBERN%\commons-collections-2.1.1.jar;%HIBERN%\dom4j-1.6.1.jar;%HIBERN%\ehcache-1.2.3.jar;%HIBERN%\jdbc2_0-stdext.jar;%HIBERN%\jta.jar;%HIBERN%\hibernate3.jar;%HIBERN%\hibernate-tools.jar;%HIBERN%\hibernate-annotations.jar;%HIBERN%\hibernate-commons-annotations.jar;%HIBERN%\hibernate-entitymanager.jar;%HIBERN%\javassist.jar;%HIBERN%\ejb3-persistence.jar;%POSTGR%\postgresql-8.3-603.jdbc3.jar;%LIB%\ij.jar;%LIB%\ujmp-complete-0.2.5.jar;..\CellTracking.jar au.com.nicta.ct.desktop.Main
+java -Xmx1024m -cp %HIBERNATE%\commons-logging-1.1.jar;%HIBERNATE%\antlr-2.7.6.jar;%HIBERNATE%\asm.jar;%HIBERNATE%\asm-attrs.jar;%HIBERNATE%\cglib-2.1.3.jar;%HIBERNATE%\commons-collections-2.1.1.jar;%HIBERNATE%\dom4j-1.6.1.jar;%HIBERNATE%\ehcache-1.2.3.jar;%HIBERNATE%\jdbc2_0-stdext.jar;%HIBERNATE%\jta.jar;%HIBERNATE%\hibernate3.jar;%HIBERNATE%\hibernate-tools.jar;%HIBERNATE%\hibernate-annotations.jar;%HIBERNATE%\hibernate-commons-annotations.jar;%HIBERNATE%\hibernate-entitymanager.jar;%HIBERNATE%\javassist.jar;%HIBERNATE%\ejb3-persistence.jar;..\lib\postgresql-8.3-603.jdbc3.jar;..\lib\ij.jar;..\lib\ujmp-complete-0.2.5.jar;..\dist\TrackAssist.jar au.com.nicta.ct.desktop.Main
+
 endlocal
